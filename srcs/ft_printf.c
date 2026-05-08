270468d6f56e648e2d0bccdf5e180526fbc08124
Neuza Teresa Do Nascimento <nedo-nas@c1r12s3.42porto.com>
2026-05-08 18:24:48 +0100

    second commit

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: nedo-nas <nedo-nas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 14:37:22 by nedo-nas          #+#    #+#             */
-/*   Updated: 2026/05/05 12:58:01 by nedo-nas         ###   ########.fr       */
+/*   Updated: 2026/05/08 18:24:30 by nedo-nas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,22 @@ static int	check_type(char c, va_list *args)
 {
 	if (c == 'c')
 		return (ft_putchar(va_arg(*args, int)));
-	// else if (c == 's')
-	// 	return (ft_putstr(va_arg(*args, char *)));
-	// else if (c == 'p')
-	// 	return (ft_puthex(va_arg(*args, void *)));
-	// else if (c == 'd')
-	// 	return (ft_putnbr(va_arg(*args, int)));
-	// else if (c == 'i')
-	// 	return (ft_putnbr(va_arg(*args, int)));
-	// else if (c == 'u')
-	// 	return (ft_putnbr_unsigned(va_arg(*args, unsigned int)));
-	// else if (c == 'x')
-	// 	return (printf_nbr_hex(va_arg(*args, unsigned int),
-	// 			"0123456789abcdef"));
-	// else if (c == 'X')
-	// 	return (printf_nbr_hex(va_arg(*args, unsigned int),
-	// 			"0123456789ABCDEF"));
-	else if (c == 'a')
-		return (ft_putstr("my awsome"));
+	else if (c == 's')
+		return (ft_putstr(va_arg(*args, char *)));
+	else if (c == 'p')
+		return (ft_puthex(va_arg(*args, void *)));
+	else if (c == 'd')
+		return (ft_putnbr(va_arg(*args, int)));
+	else if (c == 'i')
+		return (ft_putnbr(va_arg(*args, int)));
+	else if (c == 'u')
+		return (ft_putnbr_unsigned(va_arg(*args, unsigned int)));
+	else if (c == 'x')
+		return (printf_nbr_hex(va_arg(*args, unsigned int),
+				"0123456789abcdef"));
+	else if (c == 'X')
+		return (printf_nbr_hex(va_arg(*args, unsigned int),
+				"0123456789ABCDEF"));
 	else if (c == '%')
 		return (ft_putchar('%'));
 	return (0);
